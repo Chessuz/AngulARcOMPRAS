9b516e46cb01b1e6610de7abc931a02a012b9e92
Christyan <Christyan@Techpartner.com.br>
2019-08-08 17:37:25 -0300

    Commit Versao Final 2

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -5,6 +5,7 @@ import { Observable, from } from "rxjs";
 import { OverlayEventDetail } from "@ionic/core";
 import { take, tap } from "rxjs/operators";
 import { AlertController } from "@ionic/angular";
+import { LayoutServiceService } from "src/app/shared/layout-service.service";
 
 @Component({
     selector: "app-home",
@@ -15,7 +16,8 @@ export class HomePage {
     constructor(
         private variaveisService: VariaveisService,
         private databaseService: DatabaseService,
-        public alertController: AlertController
+        public alertController: AlertController,
+        private layoutServiceService: LayoutServiceService
     ) {}
 
     ionViewWillEnter() {
@@ -24,5 +26,13 @@ export class HomePage {
 
     ionViewWillLeave() {
         this.variaveisService.SetMenuIsDisabled(true);
+    }
+
+    AddTema() {
+        this.layoutServiceService.addBodyClass("cao-theme");
+    }
+
+    RemoveTema() {
+        this.layoutServiceService.removeBodyClass("cao-theme");
     }
 }
